b6c86bbf17bb573a677ca02d09261f801bb78003
Vitor-Laperriere <vtifaria@gmail.com>
2025-12-16 11:16:00 -0300

    setup front

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -13,6 +13,11 @@ async function bootstrap() {
     }),
   );
 
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  });
+
   const port = Number(process.env.PORT ?? 3001);
   await app.listen(port);
 }
