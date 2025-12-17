eeea0ac39772bd51cbe347b2388b670799e082e2
Vitor-Laperriere <vtifaria@gmail.com>
2025-12-17 07:37:45 -0300

    fix Cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -15,6 +15,8 @@ async function bootstrap() {
 
   app.enableCors({
     origin: process.env.CORS_ORIGIN?.split(',') ?? ['http://localhost:3000'],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
 
